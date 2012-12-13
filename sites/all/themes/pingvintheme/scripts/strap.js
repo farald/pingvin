93d2728cdb1440245eafcad02f671532319f7678
farald <farald@64469.no-reply.drupal.org>
2012-12-13 02:15:54 +0100

    Map overlay fix for Chrome

--- sites/all/themes/pingvintheme/scripts/strap.js
+++ sites/all/themes/pingvintheme/scripts/strap.js
@@ -2,10 +2,15 @@
 
     Drupal.behaviors.pingvin = {
         attach: function(context) {
+          //$('.openlayers_map_fullscreen').hide().show();
           $(window).resize();
           // Make the map ready.
           $('.olControlModifyFeatureItemInactive').click();
           $('.olControlDrawFeaturePointItemInactive').click();
         }
     };
+  window.onload = function ()  {
+    $('.openlayers_behavior_fullscreen_buttonItemInactive').click();
+    $('.openlayers_behavior_fullscreen_buttonItemInactive').click();
+  };
 })(jQuery);
